8947749ea5682e57ff79d4000d210607d7e9dd64
Soseh <sosehv@gmail.com>
2025-05-13 15:30:45 -0400

    Implements logic for exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: 'Surrey', annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+puts "Men's Stores:"
+@mens_stores.each do |store|
+  puts "#{store.name} - Annual Revenue: $#{store.annual_revenue}"
+end
+
+@womens_stores_under_1m = Store.where(womens_apparel: true).where('annual_revenue < ?', 1_000_000)
+
+puts "\nWomen's Stores under $1M revenue:"
+@womens_stores_under_1m.each do |store|
+  puts "#{store.name} - Annual Revenue: $#{store.annual_revenue}"
+end
