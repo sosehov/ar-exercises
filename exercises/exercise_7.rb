479a8a224eaff160e4522f4f9e403ba7c80ef85a
Soseh <sosehv@gmail.com>
2025-05-13 16:27:58 -0400

    Implements exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+
+if new_store.errors.any?
+  puts "Store creation failed due to the following validation errors:"
+  new_store.errors.full_messages.each do |message|
+    puts "- #{message}"
+  end
+else
+  puts "Store '#{new_store.name}' created successfully!"
+end
